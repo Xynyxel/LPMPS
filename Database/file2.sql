3d3e60815519b249cedaf931a9f457c32ad876fd
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2021-07-19 17:01:29 +0700

    update

---
+++ Database/file2.sql
@@ -0,0 +1,705 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307:3307
+-- Generation Time: Jul 11, 2021 at 03:53 PM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lpmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akar_masalah`
+--
+
+CREATE TABLE `akar_masalah` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `sekolah_id` int(100) NOT NULL,
+  `indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akar_masalah_master`
+--
+
+CREATE TABLE `akar_masalah_master` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `indikator`
+--
+
+CREATE TABLE `indikator` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `nomor` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `status` int(100) NOT NULL,
+  `standar_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kegiatan`
+--
+
+CREATE TABLE `kegiatan` (
+  `id` int(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `program_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota_kabupaten`
+--
+
+CREATE TABLE `kota_kabupaten` (
+  `id` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lpmp`
+--
+
+CREATE TABLE `lpmp` (
+  `id` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` int(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masalah`
+--
+
+CREATE TABLE `masalah` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `sekolah_id` int(100) NOT NULL,
+  `indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengawas`
+--
+
+CREATE TABLE `pengawas` (
+  `id` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` int(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `asal` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `sekolah_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program_rekomendasi`
+--
+
+CREATE TABLE `program_rekomendasi` (
+  `id` int(100) NOT NULL,
+  `program_id` int(100) NOT NULL,
+  `rekomendasi_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raport_kpi`
+--
+
+CREATE TABLE `raport_kpi` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `nilai_kpi` float NOT NULL,
+  `kota_kabupaten_id` int(100) NOT NULL,
+  `sub_indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raport_sekolah`
+--
+
+CREATE TABLE `raport_sekolah` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `nilai` float NOT NULL,
+  `sekolah_id` int(100) NOT NULL,
+  `sub_indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `realisasi_kerja`
+--
+
+CREATE TABLE `realisasi_kerja` (
+  `id` int(100) NOT NULL,
+  `penanggung_jawab` text NOT NULL,
+  `pemangku_kepentingan` text NOT NULL,
+  `waktu_pelaksanaan` text NOT NULL,
+  `bukti_fisik_keterangan` text NOT NULL,
+  `bukti_fisik_url` text NOT NULL,
+  `status` int(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `kegiatan_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rekomendasi`
+--
+
+CREATE TABLE `rekomendasi` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `sekolah_id` int(100) NOT NULL,
+  `indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rencana_kerja`
+--
+
+CREATE TABLE `rencana_kerja` (
+  `id` int(100) NOT NULL,
+  `indikator_kinerja` text NOT NULL,
+  `volume` text NOT NULL,
+  `biaya` float NOT NULL,
+  `sumber_daya` text NOT NULL,
+  `is_dana_bos` int(100) NOT NULL,
+  `status` int(100) NOT NULL,
+  `datetime` timestamp NOT NULL,
+  `kegiatan_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sekolah`
+--
+
+CREATE TABLE `sekolah` (
+  `id` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `telefon` varchar(100) NOT NULL,
+  `kota_kabupaten_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sekolah_pengawas`
+--
+
+CREATE TABLE `sekolah_pengawas` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `tgl_sk` date NOT NULL,
+  `nomor_sk` varchar(100) NOT NULL,
+  `sekolah_id` int(100) NOT NULL,
+  `pengawas_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siklus_periode`
+--
+
+CREATE TABLE `siklus_periode` (
+  `id` int(100) NOT NULL,
+  `siklus` int(100) NOT NULL,
+  `tanggal_mulai` date NOT NULL,
+  `tanggal_selesai` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `standar`
+--
+
+CREATE TABLE `standar` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `nomor` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `status` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_indikator`
+--
+
+CREATE TABLE `sub_indikator` (
+  `id` int(100) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `nomor` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `status` int(100) NOT NULL,
+  `indikator_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tpmps`
+--
+
+CREATE TABLE `tpmps` (
+  `id` int(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` int(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `sekolah_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akar_masalah`
+--
+ALTER TABLE `akar_masalah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `indikator_id` (`indikator_id`),
+  ADD KEY `sekolah_id` (`sekolah_id`);
+
+--
+-- Indexes for table `akar_masalah_master`
+--
+ALTER TABLE `akar_masalah_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `indikator`
+--
+ALTER TABLE `indikator`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `standar_id` (`standar_id`);
+
+--
+-- Indexes for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `program_id` (`program_id`);
+
+--
+-- Indexes for table `kota_kabupaten`
+--
+ALTER TABLE `kota_kabupaten`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lpmp`
+--
+ALTER TABLE `lpmp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `masalah`
+--
+ALTER TABLE `masalah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `indikator_id` (`indikator_id`),
+  ADD KEY `sekolah_id` (`sekolah_id`);
+
+--
+-- Indexes for table `pengawas`
+--
+ALTER TABLE `pengawas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sekolah_id` (`sekolah_id`);
+
+--
+-- Indexes for table `program_rekomendasi`
+--
+ALTER TABLE `program_rekomendasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `raport_kpi`
+--
+ALTER TABLE `raport_kpi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kota_kabupaten_id` (`kota_kabupaten_id`),
+  ADD KEY `sub_indikator_id` (`sub_indikator_id`);
+
+--
+-- Indexes for table `raport_sekolah`
+--
+ALTER TABLE `raport_sekolah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sekolah_id` (`sekolah_id`),
+  ADD KEY `sub_indikator_id` (`sub_indikator_id`);
+
+--
+-- Indexes for table `realisasi_kerja`
+--
+ALTER TABLE `realisasi_kerja`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kegiatan_id` (`kegiatan_id`);
+
+--
+-- Indexes for table `rekomendasi`
+--
+ALTER TABLE `rekomendasi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `indikator_id` (`indikator_id`),
+  ADD KEY `sekolah_id` (`sekolah_id`);
+
+--
+-- Indexes for table `rencana_kerja`
+--
+ALTER TABLE `rencana_kerja`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kegiatan_id` (`kegiatan_id`);
+
+--
+-- Indexes for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kota_kabupaten_id` (`kota_kabupaten_id`);
+
+--
+-- Indexes for table `sekolah_pengawas`
+--
+ALTER TABLE `sekolah_pengawas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sekolah_id` (`sekolah_id`),
+  ADD KEY `pengawas_id` (`pengawas_id`);
+
+--
+-- Indexes for table `siklus_periode`
+--
+ALTER TABLE `siklus_periode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `standar`
+--
+ALTER TABLE `standar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_indikator`
+--
+ALTER TABLE `sub_indikator`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `indikator_id` (`indikator_id`);
+
+--
+-- Indexes for table `tpmps`
+--
+ALTER TABLE `tpmps`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sekolah_id` (`sekolah_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `akar_masalah`
+--
+ALTER TABLE `akar_masalah`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `akar_masalah_master`
+--
+ALTER TABLE `akar_masalah_master`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kota_kabupaten`
+--
+ALTER TABLE `kota_kabupaten`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lpmp`
+--
+ALTER TABLE `lpmp`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `masalah`
+--
+ALTER TABLE `masalah`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengawas`
+--
+ALTER TABLE `pengawas`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program_rekomendasi`
+--
+ALTER TABLE `program_rekomendasi`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `raport_kpi`
+--
+ALTER TABLE `raport_kpi`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `raport_sekolah`
+--
+ALTER TABLE `raport_sekolah`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `realisasi_kerja`
+--
+ALTER TABLE `realisasi_kerja`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rekomendasi`
+--
+ALTER TABLE `rekomendasi`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rencana_kerja`
+--
+ALTER TABLE `rencana_kerja`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sekolah_pengawas`
+--
+ALTER TABLE `sekolah_pengawas`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `siklus_periode`
+--
+ALTER TABLE `siklus_periode`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `standar`
+--
+ALTER TABLE `standar`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `indikator`
+--
+ALTER TABLE `indikator`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sub_indikator`
+--
+ALTER TABLE `sub_indikator`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tpmps`
+--
+ALTER TABLE `tpmps`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `akar_masalah`
+--
+ALTER TABLE `akar_masalah`
+  ADD CONSTRAINT `akar_masalah_ibfk_1` FOREIGN KEY (`indikator_id`) REFERENCES `indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `akar_masalah_ibfk_2` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `indikator`
+--
+ALTER TABLE `indikator`
+  ADD CONSTRAINT `indikator_ibfk_1` FOREIGN KEY (`standar_id`) REFERENCES `standar` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `kegiatan`
+--
+ALTER TABLE `kegiatan`
+  ADD CONSTRAINT `kegiatan_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `program` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `masalah`
+--
+ALTER TABLE `masalah`
+  ADD CONSTRAINT `masalah_ibfk_1` FOREIGN KEY (`indikator_id`) REFERENCES `indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `masalah_ibfk_2` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `program`
+--
+ALTER TABLE `program`
+  ADD CONSTRAINT `program_ibfk_1` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `raport_kpi`
+--
+ALTER TABLE `raport_kpi`
+  ADD CONSTRAINT `raport_kpi_ibfk_1` FOREIGN KEY (`kota_kabupaten_id`) REFERENCES `kota_kabupaten` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `raport_kpi_ibfk_2` FOREIGN KEY (`sub_indikator_id`) REFERENCES `sub_indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `raport_sekolah`
+--
+ALTER TABLE `raport_sekolah`
+  ADD CONSTRAINT `raport_sekolah_ibfk_1` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `raport_sekolah_ibfk_2` FOREIGN KEY (`sub_indikator_id`) REFERENCES `sub_indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `realisasi_kerja`
+--
+ALTER TABLE `realisasi_kerja`
+  ADD CONSTRAINT `realisasi_kerja_ibfk_1` FOREIGN KEY (`kegiatan_id`) REFERENCES `kegiatan` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `rekomendasi`
+--
+ALTER TABLE `rekomendasi`
+  ADD CONSTRAINT `rekomendasi_ibfk_1` FOREIGN KEY (`indikator_id`) REFERENCES `indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `rekomendasi_ibfk_2` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `rencana_kerja`
+--
+ALTER TABLE `rencana_kerja`
+  ADD CONSTRAINT `rencana_kerja_ibfk_1` FOREIGN KEY (`kegiatan_id`) REFERENCES `kegiatan` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `sekolah`
+--
+ALTER TABLE `sekolah`
+  ADD CONSTRAINT `sekolah_ibfk_1` FOREIGN KEY (`kota_kabupaten_id`) REFERENCES `kota_kabupaten` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `sekolah_pengawas`
+--
+ALTER TABLE `sekolah_pengawas`
+  ADD CONSTRAINT `sekolah_pengawas_ibfk_1` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sekolah_pengawas_ibfk_2` FOREIGN KEY (`pengawas_id`) REFERENCES `pengawas` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `sub_indikator`
+--
+ALTER TABLE `sub_indikator`
+  ADD CONSTRAINT `sub_indikator_ibfk_1` FOREIGN KEY (`indikator_id`) REFERENCES `indikator` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tpmps`
+--
+ALTER TABLE `tpmps`
+  ADD CONSTRAINT `tpmps_ibfk_1` FOREIGN KEY (`sekolah_id`) REFERENCES `sekolah` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
